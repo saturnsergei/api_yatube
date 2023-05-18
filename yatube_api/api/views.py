d7dde91a5fe60ff93c9769f03495b9168e60766e
Сергей Савчишкин <rings.of.saturn@yandex.ru>
2023-05-18 15:44:21 +0200

    delete comments and fix get_queryset

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 
-from posts.models import Post, Group, Comment
+from posts.models import Post, Group
 from .serializers import PostSerializer, GroupSerializer, CommentSerializer
 from .permissions import IsAuthorOrAuthenticated
 
@@ -27,8 +27,7 @@ class CommentViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         post_id = self.kwargs.get("post_id")
         post = get_object_or_404(Post, pk=post_id)
-        new_queryset = Comment.objects.filter(post=post)
-        return new_queryset
+        return post.comments.all()
 
     def perform_create(self, serializer):
         post_id = self.kwargs.get("post_id")
